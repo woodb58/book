4248072ab041a53c026dfc935d6c01de04e97897
woodb58 <woodb58@gmail.com>
2025-10-01 20:32:03 -0500

    added id and function to add to library

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function Book(name, author, pages) {
+const myLibrary = [];
+
+function Book(name, author, pages, id) {
     this.name = name,
     this.author = author,
     this.pages = pages
+    this.id = crypto.randomUUID()
     this.bookInfo = function(){
         return{
            name: this.name,
@@ -11,5 +14,14 @@ function Book(name, author, pages) {
     }
 }
 
-const theHobbit = new Book("hobbit", "jrr", 508)
+function addBook(name, author, pages, id){
+    const newBook = new Book(name, author, pages, id);
+    myLibrary.push(newBook);
+
+}
+
+addBook("Hobbit", 'JRR', 8526);
+addBook("GOT", 'George', 1526);
+
+console.log(myLibrary)
 
